b84b1a04190c10f83c580c54e81187429b28f87d
Nikos Lithoxopoulos <nikoslithoxopoulos@Nikoss-Mac-mini.local>
2024-02-10 15:42:42 +0100

    add loadfiles.py

---
+++ loadfiles.py
@@ -0,0 +1,42 @@
+import pdfplumber
+import io
+import logging
+
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+
+logging.basicConfig(filename='laodfiles.log', level=logging.INFO)
+
+def file_uploader(st, st_document):
+    st.write("File has been uploaded!")
+    bytes_data = st_document.getvalue()
+    with pdfplumber.open(io.BytesIO(bytes_data)) as pdf:
+        text = ''
+        for page in pdf.pages:
+            text += page.extract_text() + "\n"
+    
+    # separators = ["\n\n", "\n", "(?<=\, )", " ", ""]
+    # pattern = '|'.join(map(re.escape, separators))
+
+    # split_text = re.split(pattern, text)
+    # separator = ' '  # Define the separator you want to use, e.g., a space
+    # my_string = separator.join(split_text)
+
+    logging.info(f"Upload Text: {text}")
+    
+
+    # add_to_collection(text)
+
+    # for segment in split_text:
+    #     if segment:  # Check if segment is not empty
+            # add_to_collection(segment)
+            # print(f"Segment: {segment}")
+        # add_to_collection(segment)  # Assuming this is a function you've defined
+        # print(f"Segment: {segment}")
+    
+    # add_to_collection(text)
+    # reader = PyPDF2.PdfReader(st_document)
+    # print(len(reader.pages))
+    # page = reader.pages[0]
+    # text = page.extract_text()
+    # print(f"Page: {page}")
+    
